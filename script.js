f02096ce2d4a7d2c9a1b1148828efceccdeaf5c6
matthew <153331629+aremumatthew@users.noreply.github.com>
2024-03-26 22:25:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+function playVideo1(){
+  let btn = document.getElementById('video-cover1');
+  let video = document.getElementById('video-image1');
+  let time = document.getElementById('video-time1');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+
+function playVideo2(){
+  let btn = document.getElementById('video-cover2');
+  let video = document.getElementById('video-image2');
+  let time = document.getElementById('video-time2');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo3(){
+  let btn = document.getElementById('video-cover3');
+  let video = document.getElementById('video-image3');
+  let time = document.getElementById('video-time3');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo4(){
+  let btn = document.getElementById('video-cover4');
+  let video = document.getElementById('video-image4');
+  let time = document.getElementById('video-time4');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo5(){
+  let btn = document.getElementById('video-cover5');
+  let video = document.getElementById('video-image5');
+  let time = document.getElementById('video-time5');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo6(){
+  let btn = document.getElementById('video-cover6');
+  let video = document.getElementById('video-image6');
+  let time = document.getElementById('video-time6');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo7(){
+  let btn = document.getElementById('video-cover7');
+  let video = document.getElementById('video-image7');
+  let time = document.getElementById('video-time7');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo8(){
+  let btn = document.getElementById('video-cover8');
+  let video = document.getElementById('video-image8');
+  let time = document.getElementById('video-time8');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo9(){
+  let btn = document.getElementById('video-cover9');
+  let video = document.getElementById('video-image9');
+  let time = document.getElementById('video-time9');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo10(){
+  let btn = document.getElementById('video-cover10');
+  let video = document.getElementById('video-image10');
+  let time = document.getElementById('video-time10');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo11(){
+  let btn = document.getElementById('video-cover11');
+  let video = document.getElementById('video-image11');
+  let time = document.getElementById('video-time11');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
+function playVideo12(){
+  let btn = document.getElementById('video-cover12');
+  let video = document.getElementById('video-image12');
+  let time = document.getElementById('video-time12');
+  
+  btn.style.opacity = 0
+  video.style.opacity = 1
+  time.style.opacity = 0
+
+}
